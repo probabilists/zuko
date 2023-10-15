8321edd1ff7312c85e6964ad09f59fad54e6d96e
François Rozet <francois.rozet@outlook.com>
2023-10-15 12:55:27 +0200

    🧪 Refactor test_flows with pytest.mark.parametrize

--- tests/test_flows.py
+++ tests/test_flows.py
@@ -4,6 +4,7 @@ import pytest
 import torch
 
 from functools import partial
+from pathlib import Path
 from torch import randn
 from zuko.flows import *
 
@@ -11,77 +12,65 @@ from zuko.flows import *
 torch.set_default_dtype(torch.float64)
 
 
-def test_flows(tmp_path):
-    Fs = [
-        GMM,
-        NICE,
-        MAF,
-        NSF,
-        SOSPF,
-        NAF,
-        UNAF,
-        CNF,
-        GF,
-    ]
+@pytest.mark.parametrize('F', [GMM, NICE, MAF, NSF, SOSPF, NAF, UNAF, CNF, GF])
+def test_flows(tmp_path: Path, F: callable):
+    flow = F(3, 5)
 
-    for F in Fs:
-        flow = F(3, 5)
+    # Evaluation of log_prob
+    x, c = randn(256, 3), randn(5)
+    log_p = flow(c).log_prob(x)
 
-        # Evaluation of log_prob
-        x, c = randn(256, 3), randn(5)
-        log_p = flow(c).log_prob(x)
+    assert log_p.shape == (256,)
+    assert log_p.requires_grad
 
-        assert log_p.shape == (256,), F
-        assert log_p.requires_grad, F
+    flow.zero_grad(set_to_none=True)
+    loss = -log_p.mean()
+    loss.backward()
+
+    for p in flow.parameters():
+        assert p.grad is not None
+
+    # Sampling
+    x = flow(c).sample((32,))
+
+    assert x.shape == (32, 3)
+
+    # Reparameterization trick
+    if flow(c).has_rsample:
+        x = flow(c).rsample()
 
         flow.zero_grad(set_to_none=True)
-        loss = -log_p.mean()
+        loss = x.square().sum().sqrt()
         loss.backward()
 
         for p in flow.parameters():
-            assert p.grad is not None, F
+            assert p.grad is not None
 
-        # Sampling
-        x = flow(c).sample((32,))
+    # Invertibility
+    if isinstance(flow, Flow):
+        x, c = randn(256, 3), randn(256, 5)
+        t = flow(c).transform
+        z = t.inv(t(x))
 
-        assert x.shape == (32, 3), F
+        assert torch.allclose(x, z, atol=1e-4)
 
-        # Reparameterization trick
-        if flow(c).has_rsample:
-            x = flow(c).rsample()
+    # Saving
+    torch.save(flow, tmp_path / 'flow.pth')
 
-            flow.zero_grad(set_to_none=True)
-            loss = x.square().sum().sqrt()
-            loss.backward()
+    # Loading
+    flow_bis = torch.load(tmp_path / 'flow.pth')
 
-            for p in flow.parameters():
-                assert p.grad is not None, F
+    x, c = randn(3), randn(5)
 
-        # Invertibility
-        if isinstance(flow, Flow):
-            x, c = randn(256, 3), randn(256, 5)
-            t = flow(c).transform
-            z = t.inv(t(x))
+    seed = torch.seed()
+    log_p = flow(c).log_prob(x)
+    torch.manual_seed(seed)
+    log_p_bis = flow_bis(c).log_prob(x)
 
-            assert torch.allclose(x, z, atol=1e-4), F
+    assert torch.allclose(log_p, log_p_bis)
 
-        # Saving
-        torch.save(flow, tmp_path / 'flow.pth')
-
-        # Loading
-        flow_bis = torch.load(tmp_path / 'flow.pth')
-
-        x, c = randn(3), randn(5)
-
-        seed = torch.seed()
-        log_p = flow(c).log_prob(x)
-        torch.manual_seed(seed)
-        log_p_bis = flow_bis(c).log_prob(x)
-
-        assert torch.allclose(log_p, log_p_bis), F
-
-        # Printing
-        assert repr(flow), F
+    # Printing
+    assert repr(flow)
 
 
 def test_triangular_transforms():
