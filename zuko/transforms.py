3d194f0d3957036ac8cbee20b1b975da8bc7d92b
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2024-01-29 18:19:51 +0100

    add additional reference to BNF density forecasting paper
    adds additional reference to BNF density forecasting paper

--- zuko/transforms.py
+++ zuko/transforms.py
@@ -609,6 +609,8 @@ class BernsteinTransform(MonotonicTransform):
     the sigmoid function.
 
     References:
+        | Deep transformation models: Tackling complex regression problems with neural network based transformation models (Sick et al., 2020)
+        | https://arxiv.org/abs/2004.00464
         | Short-Term Density Forecasting of Low-Voltage Load using Bernstein-Polynomial Normalizing Flows (Arpogaus et al., 2022)
         | https://arxiv.org/abs/2204.13939
 
