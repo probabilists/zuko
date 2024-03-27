ecf0773b2776e10192f4888d16e414afaf3e5c67
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2024-03-27 17:51:59 +0100

    remove unnecessary use of partial and add original paper as reference

--- zuko/flows/polynomial.py
+++ zuko/flows/polynomial.py
@@ -5,7 +5,6 @@ __all__ = [
     "BPF",
 ]
 
-from functools import partial
 
 # isort: local
 from .autoregressive import MAF
@@ -70,6 +69,9 @@ class BPF(MAF):
         :class:`zuko.transforms.BernsteinTransform`
 
     References:
+        | Deep transformation models: Tackling complex regression problems with neural network based transformation models (Sick et al., 2020)
+        | https://arxiv.org/abs/2004.00464
+
         | Short-Term Density Forecasting of Low-Voltage Load using Bernstein-Polynomial Normalizing Flows (Arpogaus et al., 2022)
         | https://arxiv.org/abs/2204.13939
 
@@ -90,7 +92,7 @@ class BPF(MAF):
         super().__init__(
             features=features,
             context=context,
-            univariate=partial(BoundedBernsteinTransform),
+            univariate=BoundedBernsteinTransform,
             shapes=[(degree + 1,)],
             **kwargs,
         )
