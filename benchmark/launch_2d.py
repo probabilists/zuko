d3f35a0b528aafb4bd413dc1ee44ebcfa328054a
François Rozet <francois.rozet@outlook.com>
2025-10-02 00:58:38 +0200

    🎨 Format code

--- benchmark/launch_2d.py
+++ benchmark/launch_2d.py
@@ -7,22 +7,26 @@ import numpy as np
 import time
 import torch
 import wandb
-import zuko
 
-from dawgz import job, context, schedule
+from dawgz import job, schedule
 from functools import partial
 from PIL import Image
 from tqdm import tqdm
+
+import zuko
 
 
 def two_moons(n: int, sigma: float = 1e-1) -> np.ndarray:
     theta = 2 * np.pi * np.random.rand(n)
     shift = (theta > np.pi) - 1 / 2
 
-    x = np.stack((
-        np.cos(theta) + shift,
-        np.sin(theta) + shift / 2,
-    ), axis=-1)
+    x = np.stack(
+        (
+            np.cos(theta) + shift,
+            np.sin(theta) + shift / 2,
+        ),
+        axis=-1,
+    )
 
     return x + sigma * np.random.randn(n, 2)
 
@@ -31,10 +35,13 @@ def two_circles(n: int, sigma: float = 1e-1) -> np.ndarray:
     theta = 2 * np.pi * np.random.rand(n)
     r = np.where(np.random.rand(n) < 0.5, 0.75, 1.5)
 
-    x = np.stack((
-        r * np.cos(theta),
-        r * np.sin(theta),
-    ), axis=-1)
+    x = np.stack(
+        (
+            r * np.cos(theta),
+            r * np.sin(theta),
+        ),
+        axis=-1,
+    )
 
     return x + sigma * np.random.randn(n, 2)
 
@@ -43,10 +50,13 @@ def two_spirals(n: int, sigma: float = 5e-2) -> np.ndarray:
     theta = 3 * np.pi * np.sqrt(np.random.rand(n))
     r = theta / 2 / np.pi * np.sign(np.random.randn(n))
 
-    x = np.stack((
-        r * np.cos(theta),
-        r * np.sin(theta),
-    ), axis=-1)
+    x = np.stack(
+        (
+            r * np.cos(theta),
+            r * np.sin(theta),
+        ),
+        axis=-1,
+    )
 
     return x + sigma * np.random.randn(n, 2)
 
@@ -54,15 +64,18 @@ def two_spirals(n: int, sigma: float = 5e-2) -> np.ndarray:
 def eight_gaussians(n: int, sigma: float = 1e-1) -> np.ndarray:
     theta = 2 * np.pi * np.random.randint(0, 8, n) / 8
 
-    x = np.stack((
-        np.cos(theta),
-        np.sin(theta),
-    ), axis=-1)
+    x = np.stack(
+        (
+            np.cos(theta),
+            np.sin(theta),
+        ),
+        axis=-1,
+    )
 
     return x + sigma * np.random.randn(n, 2)
 
 
-def pinwheel(n: int) ->  np.ndarray:
+def pinwheel(n: int) -> np.ndarray:
     theta = 2 * np.pi * np.random.randint(0, 5, n) / 5
 
     a = 0.25 * np.random.randn(n) + 1
@@ -70,10 +83,13 @@ def pinwheel(n: int) ->  np.ndarray:
 
     theta = theta + np.exp(a - 1)
 
-    x = np.stack((
-        a * np.cos(theta) - b * np.sin(theta),
-        a * np.sin(theta) + b * np.cos(theta),
-    ), axis=-1)
+    x = np.stack(
+        (
+            a * np.cos(theta) - b * np.sin(theta),
+            a * np.sin(theta) + b * np.cos(theta),
+        ),
+        axis=-1,
+    )
 
     return x
 
@@ -88,38 +104,38 @@ def checkerboard(n: int) -> np.ndarray:
 
 
 FLOWS = {
-    'GMM': partial(zuko.flows.GMM, components=16),
-    'MAF': zuko.flows.MAF,
-    'NAF': zuko.flows.NAF,
-    'UNAF': zuko.flows.UNAF,
-    'SOSPF': zuko.flows.SOSPF,
-    'NSF': zuko.flows.NSF,
-    'FFJORD': zuko.flows.CNF,
-    'GF': zuko.flows.GF,
+    "GMM": partial(zuko.flows.GMM, components=16),
+    "MAF": zuko.flows.MAF,
+    "NAF": zuko.flows.NAF,
+    "UNAF": zuko.flows.UNAF,
+    "SOSPF": zuko.flows.SOSPF,
+    "NSF": zuko.flows.NSF,
+    "FFJORD": zuko.flows.CNF,
+    "GF": zuko.flows.GF,
 }
 
 DATASETS = {
-    'moons': two_moons,
-    'circles': two_circles,
-    'spirals': two_spirals,
-    'gaussians': eight_gaussians,
-    'pinwheel': pinwheel,
-    'checkerboard': checkerboard,
+    "moons": two_moons,
+    "circles": two_circles,
+    "spirals": two_spirals,
+    "gaussians": eight_gaussians,
+    "pinwheel": pinwheel,
+    "checkerboard": checkerboard,
 }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--transforms', type=int, default=3)
-    parser.add_argument('--hidden-features', type=int, nargs='+', default=(64, 64))
-    parser.add_argument('--residual', default=False, action='store_true')
-    parser.add_argument('--samples', type=int, default=16384)
-    parser.add_argument('--learning-rate', type=float, default=1e-3)
-    parser.add_argument('--weight-decay', type=float, default=1e-4)
-    parser.add_argument('--epochs', type=int, default=64)
-    parser.add_argument('--batch-size', type=int, default=16)
-    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument("--transforms", type=int, default=3)
+    parser.add_argument("--hidden-features", type=int, nargs="+", default=(64, 64))
+    parser.add_argument("--residual", default=False, action="store_true")
+    parser.add_argument("--samples", type=int, default=16384)
+    parser.add_argument("--learning-rate", type=float, default=1e-3)
+    parser.add_argument("--weight-decay", type=float, default=1e-4)
+    parser.add_argument("--epochs", type=int, default=64)
+    parser.add_argument("--batch-size", type=int, default=16)
+    parser.add_argument("--seed", type=int, default=0)
 
     args = parser.parse_args()
 
@@ -127,9 +143,9 @@ if __name__ == '__main__':
 
     for arch, build in FLOWS.items():
         for dataset, sample in DATASETS.items():
-            @context(arch=arch, build=build, dataset=dataset, sample=sample)
-            @job(name=f'{arch}: {dataset}', cpus=4, ram='16GB', time='06:00:00')
-            def experiment():
+
+            @job(name=f"{arch}: {dataset}", cpus=4, ram="16GB", time="06:00:00")
+            def experiment(arch=arch, build=build, dataset=dataset, sample=sample):
                 np.random.seed(args.seed)
                 torch.manual_seed(args.seed)
 
@@ -138,22 +154,22 @@ if __name__ == '__main__':
                 testset = torch.from_numpy(sample(args.samples)).float()
 
                 # Flow
-                if 'transforms' in inspect.signature(build).parameters:
+                if "transforms" in inspect.signature(build).parameters:
                     kwargs = dict(transforms=args.transforms, hidden_features=args.hidden_features)
                 else:
                     kwargs = dict(hidden_features=args.hidden_features)
 
                 try:
                     flow = build(trainset.shape[-1], residual=args.residual, **kwargs)
-                except:
+                except TypeError:
                     flow = build(trainset.shape[-1], **kwargs)
-                finally:
-                    pass
 
-                optimizer = torch.optim.AdamW(flow.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+                optimizer = torch.optim.AdamW(
+                    flow.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay
+                )
 
                 run = wandb.init(
-                    project='zuko-benchmark-2d',
+                    project="zuko-benchmark-2d",
                     config=dict(arch=arch, dataset=dataset, **vars(args)),
                 )
 
@@ -191,21 +207,23 @@ if __name__ == '__main__':
                         x = flow().sample((args.samples,))
                         end = time.perf_counter()
 
-                        h, _, _ = np.histogram2d(*x.T, bins=64, range=((-2, 2), (-2, 2)), density=True)
+                        h, _, _ = np.histogram2d(
+                            *x.T, bins=64, range=((-2, 2), (-2, 2)), density=True
+                        )
 
                         img = plt.cm.ScalarMappable().to_rgba(h, bytes=True)
                         img = Image.fromarray(img)
 
                         run.log({
-                            'train_loss': train_loss,
-                            'test_loss': test_loss,
-                            'speed': args.samples / (end - start),
-                            'histogram': wandb.Image(img),
+                            "train_loss": train_loss,
+                            "test_loss": test_loss,
+                            "speed": args.samples / (end - start),
+                            "histogram": wandb.Image(img),
                         })
                     else:
                         run.log({
-                            'train_loss': train_loss,
-                            'test_loss': test_loss,
+                            "train_loss": train_loss,
+                            "test_loss": test_loss,
                         })
 
                 run.finish()
@@ -214,8 +232,8 @@ if __name__ == '__main__':
 
     schedule(
         *jobs,
-        name='Benchmark 2D',
-        backend='slurm',
-        export='ALL',
-        env=['export WANDB_SILENT=true'],
+        name="Benchmark 2D",
+        backend="slurm",
+        export="ALL",
+        env=["export WANDB_SILENT=true"],
     )
--- benchmark/launch_uci.py
+++ benchmark/launch_uci.py
@@ -5,63 +5,67 @@ import inspect
 import numpy as np
 import torch
 import wandb
-import zuko
 
-from dawgz import job, context, schedule
+from dawgz import context, job, schedule
 from functools import partial
 from tqdm import tqdm
 
+import zuko
+
 
 def power():
-    return np.load('POWER.npy')
+    return np.load("POWER.npy")
+
 
 def gas():
-    return np.load('GAS.npy')
+    return np.load("GAS.npy")
+
 
 def hepmass():
-    return np.load('HEPMASS.npy')
+    return np.load("HEPMASS.npy")
+
 
 def bsds300():
-    return np.load('BSDS300.npy') * 10
+    return np.load("BSDS300.npy") * 10
 
 
 FLOWS = {
-    'GMM': partial(zuko.flows.GMM, components=16),
-    'MAF (AR)': zuko.flows.MAF,
-    'MAF (C)': partial(zuko.flows.MAF, passes=2),
-    'NAF (AR)': zuko.flows.NAF,
-    'NAF (C)': partial(zuko.flows.NAF, passes=2),
-    'UNAF (AR)': zuko.flows.UNAF,
-    'UNAF (C)': partial(zuko.flows.UNAF, passes=2),
-    'SOSPF (AR)': zuko.flows.SOSPF,
-    'SOSPF (C)': partial(zuko.flows.SOSPF, passes=2),
-    'NSF (AR)': zuko.flows.NSF,
-    'NSF (C)': partial(zuko.flows.NSF, passes=2),
-    'FFJORD': partial(zuko.flows.CNF, exact=False),
-    'GF': zuko.flows.GF,
+    "GMM": partial(zuko.flows.GMM, components=16),
+    "MAF (AR)": zuko.flows.MAF,
+    "MAF (C)": partial(zuko.flows.MAF, passes=2),
+    "NAF (AR)": zuko.flows.NAF,
+    "NAF (C)": partial(zuko.flows.NAF, passes=2),
+    "UNAF (AR)": zuko.flows.UNAF,
+    "UNAF (C)": partial(zuko.flows.UNAF, passes=2),
+    "SOSPF (AR)": zuko.flows.SOSPF,
+    "SOSPF (C)": partial(zuko.flows.SOSPF, passes=2),
+    "NSF (AR)": zuko.flows.NSF,
+    "NSF (C)": partial(zuko.flows.NSF, passes=2),
+    "FFJORD": partial(zuko.flows.CNF, exact=False),
+    "GF": zuko.flows.GF,
 }
 
 DATASETS = {
-    'POWER': power,
-    'GAS': gas,
-    'HEPMASS': hepmass,
-    'BSDS300': bsds300,
+    "POWER": power,
+    "GAS": gas,
+    "HEPMASS": hepmass,
+    "BSDS300": bsds300,
 }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--transforms', type=int, default=3)
-    parser.add_argument('--randperm', default=False, action='store_true')
-    parser.add_argument('--hidden-features', type=int, nargs='+', default=(512, 512))
-    parser.add_argument('--residual', default=False, action='store_true')
-    parser.add_argument('--learning-rate', type=float, default=1e-3)
-    parser.add_argument('--weight-decay', type=float, default=1e-4)
-    parser.add_argument('--epochs', type=int, default=256)
-    parser.add_argument('--epoch-size', type=int, default=65536)
-    parser.add_argument('--batch-size', type=int, default=256)
-    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument("--transforms", type=int, default=3)
+    parser.add_argument("--randperm", default=False, action="store_true")
+    parser.add_argument("--hidden-features", type=int, nargs="+", default=(512, 512))
+    parser.add_argument("--residual", default=False, action="store_true")
+    parser.add_argument("--learning-rate", type=float, default=1e-3)
+    parser.add_argument("--weight-decay", type=float, default=1e-4)
+    parser.add_argument("--epochs", type=int, default=256)
+    parser.add_argument("--epoch-size", type=int, default=65536)
+    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--seed", type=int, default=0)
 
     args = parser.parse_args()
 
@@ -69,9 +73,8 @@ if __name__ == '__main__':
 
     for arch, build in FLOWS.items():
         for dataset, load in DATASETS.items():
-            @context(arch=arch, build=build, dataset=dataset, load=load)
-            @job(name=f'{arch}: {dataset}', cpus=4, gpus=1, ram='16GB', time='24:00:00')
-            def experiment():
+            @job(name=f"{arch}: {dataset}", cpus=4, gpus=1, ram="16GB", time="24:00:00")
+            def experiment(arch=arch, build=build, dataset=dataset, load=load):
                 np.random.seed(args.seed)
                 torch.manual_seed(args.seed)
 
@@ -83,23 +86,30 @@ if __name__ == '__main__':
                 testset = torch.from_numpy(data[split:]).float()
 
                 # Flow
-                if 'transforms' in inspect.signature(build).parameters:
+                if "transforms" in inspect.signature(build).parameters:
                     kwargs = dict(transforms=args.transforms, hidden_features=args.hidden_features)
                 else:
                     kwargs = dict(hidden_features=args.hidden_features)
 
                 try:
-                    flow = build(trainset.shape[-1], randperm=args.randperm, residual=args.residual, **kwargs)
-                except:
+                    flow = build(
+                        trainset.shape[-1],
+                        randperm=args.randperm,
+                        residual=args.residual,
+                        **kwargs,
+                    )
+                except TypeError:
                     flow = build(trainset.shape[-1], **kwargs)
                 finally:
                     flow.cuda()
 
-                optimizer = torch.optim.AdamW(flow.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay)
+                optimizer = torch.optim.AdamW(
+                    flow.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay
+                )
                 scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, 1.0, 0.0, args.epochs)
 
                 run = wandb.init(
-                    project='zuko-benchmark-uci',
+                    project="zuko-benchmark-uci",
                     config=dict(arch=arch, dataset=dataset, **vars(args)),
                 )
 
@@ -110,7 +120,7 @@ if __name__ == '__main__':
 
                     flow.train()
 
-                    order = torch.randperm(len(trainset))[:args.epoch_size]
+                    order = torch.randperm(len(trainset))[: args.epoch_size]
 
                     for x in trainset[order].split(args.batch_size):
                         x = x.cuda(non_blocking=True)
@@ -147,14 +157,14 @@ if __name__ == '__main__':
                         torch.cuda.synchronize()
 
                         run.log({
-                            'train_loss': train_loss,
-                            'test_loss': test_loss,
-                            'speed': 4096 / start.elapsed_time(end),
+                            "train_loss": train_loss,
+                            "test_loss": test_loss,
+                            "speed": 4096 / start.elapsed_time(end),
                         })
                     else:
                         run.log({
-                            'train_loss': train_loss,
-                            'test_loss': test_loss,
+                            "train_loss": train_loss,
+                            "test_loss": test_loss,
                         })
 
                     scheduler.step()
@@ -165,8 +175,8 @@ if __name__ == '__main__':
 
     schedule(
         *jobs,
-        name='Benchmark UCI',
-        backend='slurm',
-        export='ALL',
-        env=['export WANDB_SILENT=true'],
+        name="Benchmark UCI",
+        backend="slurm",
+        export="ALL",
+        env=["export WANDB_SILENT=true"],
     )
